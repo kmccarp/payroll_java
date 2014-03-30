de8fcfa148fe1985a85d0e95ad535aaf92e78461
Kevin Carpenter <kwcarpenter@gmail.com>
2014-03-30 00:52:23 -0500

    verification that it's hitting the controllers

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,21 +1,21 @@
 var controllers = angular.module('controllers', [])
 
 controllers.controller('MainCtrl', function($scope) {
-	
+	console.log('hit main controller')
 })
 
 controllers.controller('TimesheetCtrl', function($scope, Restangular) {
-	
+	console.log('hit timesheet controller')
 })
 
 controllers.controller('EmployeeTimesheetCtrl', function($scope, Restangular) {
-	
+	console.log('hit emp timesheet controller')
 })
 
 controllers.controller('ManageEmployeeCtrl', function($scope, Restangular) {
-	
+	console.log('hit manage emps controller')
 })
 
 controllers.controller('SettingsCtrl', function($scope, Restangular) {
-	
+	console.log('hit settings controller')
 })
